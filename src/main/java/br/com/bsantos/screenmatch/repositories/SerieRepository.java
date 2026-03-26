7465cdee639894a999bd941d76334b3fbe6a4080
brunorsnts <santos_2002@outlook.com.br>
2026-03-26 17:13:16 -0300

    Adiciona anotacao @Param em metodos que usam a JPQL

--- src/main/java/br/com/bsantos/screenmatch/repositories/SerieRepository.java
+++ src/main/java/br/com/bsantos/screenmatch/repositories/SerieRepository.java
@@ -5,6 +5,7 @@ import br.com.bsantos.screenmatch.models.Episodio;
 import br.com.bsantos.screenmatch.models.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,14 +23,14 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
     List<Serie> findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(int maxTemporada, double avaliacaoMin);
 
     @Query("SELECT s FROM Serie s WHERE s.totalTemporadas <= :maxTemporada AND s.avaliacao >= :avaliacaoMin")
-    List<Serie> filtrarMaxTemporadasEMinAvaliacao(int maxTemporada, double avaliacaoMin);
+    List<Serie> filtrarMaxTemporadasEMinAvaliacao(@Param("maxTemporada") int maxTemporada, @Param("avaliacaoMin") double avaliacaoMin);
 
     @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE e.titulo ILIKE %:trecho%")
-    List<Episodio> buscarEpisodiosPorTrecho(String trecho);
+    List<Episodio> buscarEpisodiosPorTrecho(@Param(("trecho")) String trecho);
 
     @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s = :serieBuscada ORDER BY e.avaliacao DESC LIMIT 5")
-    List<Episodio> topEpisodiosSerie(Serie serieBuscada);
+    List<Episodio> topEpisodiosSerie(@Param("serieBuscada") Serie serieBuscada);
 
     @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s = :serieBuscada AND YEAR(e.dataLancamento) >= :ano")
-    List<Episodio> filtraEpisodiosDeUmaSerieApartirDeUmaData(Serie serieBuscada, int ano);
+    List<Episodio> filtraEpisodiosDeUmaSerieApartirDeUmaData(@Param("serieBuscada") Serie serieBuscada, @Param("ano") int ano);
 }
